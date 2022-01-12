a6ddbaee2e5d83f94897512fcdcfb2f2638168dc
Ankush Chavan <ankush.chavan@velotio.com>
2022-01-12 11:52:53 +0530

    (Init) Init
    Signed-off-by: Ankush Chavan <ankush.chavan@velotio.com>

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    """This function return hello world"""
+
+    return {"message": "Hello World"}
+
+
+# Fixed path routes must be declared before the routes with path parameters
+# to avoid the conflicts
+@app.get("/hello/me")
+async def say_hello_to_me():
+    """This function says hello to current user"""
+
+    return {"message": "Hello current user"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    """This function says hello"""
+
+    return {"message": f"Hello {name}"}
+
+
+class User(BaseModel):
+    name: str
+    email: str
+
+
+@app.post("/user")
+async def create_user(user: User):
+    """This function creates new user"""
+
+    return user
+
+
+@app.put("/user/{user_id}")
+async def update_user(user_id: int, user: User, is_admin: bool = Body(...)):
+    """This function updates the user"""
+
+    return {"item_id": user_id, **user.dict(), "is_admin": is_admin}
